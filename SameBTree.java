43e8e985632714343c27ddde6ed77a6e2aa5c3dd
nanjappan <nanjappan@users.noreply.github.com>
2021-02-09 18:13:45 -0700

    Create SameBTree.java

---
+++ SameBTree.java
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/same-tree/
+// Easy
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        if (p==null)
+        {
+            if (q != null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }else if (q==null)
+        {
+            return false;
+        }else if (p.val != q.val)
+        {
+            return false;
+        }
+        
+       
+        if (!isSameTree(p.left,q.left)) return false;
+        if (!isSameTree(p.right,q.right)) return false;
+        return true;
+    }
+}
